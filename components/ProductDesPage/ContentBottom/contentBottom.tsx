88065ea6126d002e09c6cf4c92579bed40f47b92
Vishal <vishalanand072@gmail.com>
2023-09-17 02:08:10 +0530

    A. Content Botom B. Reasponsive: Web Order: 1. Left (Tabs Detail, Delivery etc) 2. Center : Image 3.  Product Ad to Cart Mobile Order: 1. Image 2. Add to cart 3. Details

--- components/ProductDesPage/ContentBottom/contentBottom.tsx
+++ components/ProductDesPage/ContentBottom/contentBottom.tsx
@@ -1,5 +1,38 @@
+import { Link } from "@nextui-org/react";
+
 const ContentBottom = () => {
-    return (  );
-}
- 
+  return (
+    <>
+      <div className="flex justify-center gap-4  py-4">
+        <div className="text-center uppercase whitespace-nowrap">
+          <Link color="foreground" href="#" underline="always" className="pl-1">
+            Jonathan Simkhai
+          </Link>
+        </div>
+        <div className=" uppercase whitespace-nowrap">
+          <Link color="foreground" href="#" underline="always" className="pl-1">
+            Blazers
+          </Link>
+        </div>
+        <div className="text-center uppercase whitespace-nowrap">
+          <Link color="foreground" href="#" underline="always" className="pl-1">
+            Viscose
+          </Link>
+        </div>
+      </div>
+      <div className="font-bold uppercase font-helvetica pt-20 py-4">
+        a note from the editor
+      </div>
+      <div className="text-[32px] -lg:text-[20px] pt-4">
+        The Forte Lurex Linen Viscose Jacket in Mother of Pearl features lunar
+        lavishness by night and by day: a blazer in a linen blend shot with
+        lurex for a shimmering surface that shines like a star in the sky.{" "}
+      </div>
+      <div className="py-4">
+        By <u>MINNA SHIM</u>, Fashion Editor
+      </div>
+    </>
+  );
+};
+
 export default ContentBottom;
--- components/ProductDesPage/index.tsx
+++ components/ProductDesPage/index.tsx
@@ -1,3 +1,4 @@
+import ContentBottom from "./ContentBottom/contentBottom";
 import PDA_ConetentLeft from "./contentLeft/contentLeft";
 import PDA_ContentMiddle from "./contentMiddle/contentMiddle";
 import PDA_ContentRight from "./contentRight/contentRight";
@@ -6,17 +7,21 @@ const Product_Page = () => {
   return (
     <>
       <div className="conatiner px-10 w-full mx-auto sm:gap-5 -lg:gap-0 grid grid-cols-4 sm:grid-cols-4 -lg:grid-cols-1 text-center transition-all ease-in-out delay-1000 ">
-        <div className="-lg:hidden">
+        <div className="-lg:order-1">
           <PDA_ConetentLeft></PDA_ConetentLeft>
         </div>
-        <div className="col-span-2">
+        <div className="col-span-2 ">
           <PDA_ContentMiddle></PDA_ContentMiddle>
         </div>
-        <div className="md:pt-18 -lg:pt-2">
+        <div className="md:pt-18 -lg:pt-2 -lg:col-span-2">
           <PDA_ContentRight></PDA_ContentRight>
         </div>
       </div>
-      <div className="col-span-2"></div>
+      <div className="grid grid-cols-4 gap-4">
+        <div className="lg:col-start-2 lg:col-span-2 -lg:col-span-4 -lg:px-8 text-center">
+          <ContentBottom></ContentBottom>
+        </div>
+      </div>
     </>
   );
 };
