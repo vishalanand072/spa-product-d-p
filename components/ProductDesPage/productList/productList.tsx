d2a1cbc011df37137ee7c0a1bc8be42f2330abf1
Vishal <vishalanand072@gmail.com>
2023-09-17 13:30:24 +0530

    Comements for Logics

--- components/ProductDesPage/productList/productList.tsx
+++ components/ProductDesPage/productList/productList.tsx
@@ -4,4 +4,7 @@ export const photos = [
   { src: "../assets/ProductImage/Image-3.jpg" },
   { src: "../assets/ProductImage/Image-4.jpg" },
   { src: "../assets/ProductImage/Image-5.jpg" },
+  { src: "../assets/ProductImage/Image-3.jpg" },
+  { src: "../assets/ProductImage/Image-4.jpg" },
+  { src: "../assets/ProductImage/Image-5.jpg" },
 ];
